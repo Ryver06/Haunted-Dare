3ceb7d272839f6566dd331b6a26e41f873fcd888
Ryver06 <Ryver.Schlienkamp@gmail.com>
2026-05-08 13:12:00 +0200

    timeline yay

---
+++ Assets/+++Workdata/Scripts/voicelines/VoicelineManager.cs
@@ -0,0 +1,12 @@
+using FMODUnity;
+using UnityEngine;
+
+public class VoicelineManager : MonoBehaviour
+{
+    public void PlayTest()
+    {
+        RuntimeManager.PlayOneShot("event:/Twst");
+    }
+    
+    
+}
