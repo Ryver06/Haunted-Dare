09ef04765c8ab7b3da541c234740f8cfd3ab1721
Ryver06 <Ryver.Schlienkamp@gmail.com>
2026-06-05 11:47:00 +0200

    asked nico for help

--- Assets/+++Workdata/Scripts/voicelines/VoicelineManager.cs
+++ Assets/+++Workdata/Scripts/voicelines/VoicelineManager.cs
@@ -1,13 +1,40 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using FMODUnity;
 using TMPro;
 using UnityEngine;
 
 public class VoicelineManager : MonoBehaviour
 {
+    // in seperates script
+    [Serializable]
+    public class StoryLineOption
+    {
+        public string key;
+        
+        //TODO maybe inky story text object?!
+        public string dialogStartText;
+        public string fmodEventId;
+    }
+
+    [SerializeField]
+    private List<StoryLineOption> _storyLineOptions = new List<StoryLineOption>();
+    
     [SerializeField] private TMP_Text dialogueTxt;
 
+    
+    //das bleibt
+    public void SetStoryline(string key)
+    {
+        StoryLineOption foundOption = _storyLineOptions.Find(option => option.key == key);
+        if (foundOption == null)
+            return; //TODO also log error!
+        
+        ReplaceSubtitles(foundOption.dialogStartText);
+        PlayVoiceline(foundOption.fmodEventId);
+    }
+    
     private void Awake()
     {
         dialogueTxt.text = "";
