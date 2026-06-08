b8c195818acf832597c7a94b86b13edf604c8cf9
Ryver06 <Ryver.Schlienkamp@gmail.com>
2026-06-08 16:19:50 +0200

    Locker beginning

---
+++ Assets/+++Workdata/Scripts/Manager/LockerManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class LockerManager : MonoBehaviour
+{
+    [Serializable]
+    public class LockerTimeline
+    {
+        public int id;
+        public PlayableDirector cutscene;
+        public Transform tp_pos;
+    }
+
+    [SerializeField] private Transform player;
+    public List<LockerTimeline> timeline = new List<LockerTimeline>();
+
+    public void checkId(int lockerId)
+    {
+        //checks if id of locker matches to the cutscene id
+        for (int i = 0; i < timeline.Count; i++)
+        {
+            //if match is found -> player Hides in locker
+            if (timeline[i].id == lockerId)
+            {
+                HideInLocker(i); //saves which object in the list it is
+            }
+        }
+    }
+
+    /// <summary>
+    /// player hides in the locker and cutscene is started
+    /// </summary>
+    private void HideInLocker(int id)
+    {
+        player = timeline[id].tp_pos;
+        timeline[id].cutscene.Play();
+    }
+}
